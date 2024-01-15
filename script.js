ed6a4bf5d47d0693e5ce9553e26fd835aedc8ced
Rafael <rafaelxvrr@gmail.com>
2024-01-15 16:55:51 -0300

    function js create

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
+const inputElement = document.querySelector("#password");
+let passwordLength = 16;
+
 function generatePassword() {
   const chars = "abcdefghjklmpqrstuvxyz123456789?!@$&*";
   let password = "";
 
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < passwordLength; i++) {
     const randomNumber = Math.floor(Math.random() * chars.length);
     password += chars.substring(randomNumber, randomNumber + 1);
   }
-  console.log(password);
+
+  inputElement.value = password;
 }
+
+function copy() {
+  navigator.clipboard.writeText(inputElement.value);
+}
+
+const passwordLengthElement = document.querySelector("#password-length");
+passwordLengthElement.addEventListener("input", function () {
+  passwordLength = passwordLengthElement.value;
+  generatePassword();
+});
+
+const copyButtonElement = document.querySelector("#copy");
+copyButtonElement.addEventListener("click", copy);
 
 generatePassword();
