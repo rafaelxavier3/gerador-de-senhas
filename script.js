52c2dfd75e4d231d19b841efd843e6603423f51f
Rafael <rafaelxvrr@gmail.com>
2024-01-15 16:35:55 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function generatePassword() {
+  const chars = "abcdefghjklmpqrstuvxyz123456789?!@$&*";
+  let password = "";
+
+  for (let i = 0; i < 8; i++) {
+    const randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);
+  }
+  console.log(password);
+}
+
+generatePassword();
